99534a441825bade9e330e9beb22cf2a19691800
Onur BÜBER <onurbuber@engineer.com>
2022-07-28 20:16:46 +0300

    build: added tuxedo-color-changer to build file

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 pkgname=tuxedo-keyboard-tools
+pkgver=1.1
+pkgrel=1
+arch=amd64
 srcdir=$(pwd)
-pkgdir=${pkgname}_1.1-1_amd64
+pkgdir=${pkgname}_${pkgver}-${pkgrel}_${arch}
 
 make
-mkdir -p ${pkgdir} ${pkgdir}/etc ${pkgdir}/etc/sudoers.d ${pkgdir}/etc/systemd/system ${pkgdir}/etc/systemd/user ${pkgdir}/opt ${pkgdir}/opt/tuxedo-keyboard-tools
+mkdir -p ${pkgdir} ${pkgdir}/usr ${pkgdir}/usr/bin ${pkgdir}/etc ${pkgdir}/etc/sudoers.d ${pkgdir}/etc/systemd/system ${pkgdir}/etc/systemd/user ${pkgdir}/opt ${pkgdir}/opt/tuxedo-keyboard-tools
 
 cp -rf ${srcdir}/DEBIAN ${pkgdir} 
 cp -rf ${srcdir}/idle ${pkgdir}/opt/tuxedo-keyboard-tools
+cp -rf ${srcdir}/tuxedo-color-changer ${pkgdir}/usr/bin
 cp -rf ${srcdir}/idle.sh ${pkgdir}/opt/tuxedo-keyboard-tools
 cp -rf ${srcdir}/keep.sh ${pkgdir}/opt/tuxedo-keyboard-tools
 cp -rf ${srcdir}/tuxedo-keyboard-idle ${pkgdir}/etc/sudoers.d
